fe3ba214cc8b518c42f8eb83c22afe00eee83434
sugoiJG <jasongolliet@icloud.com>
2023-12-16 18:26:46 -0800

    added MongoDB and mongoose to node js, and created book model with mongoose

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+
+export const mongoDBURL = 
+'mongodb+srv://jasongolliet:hoET1Bme1CgRFOMd@bookstore-mern.dmnanr5.mongodb.net/books-collection?retryWrites=true&w=majority'
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,22 @@
 import express from 'express';
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from 'mongoose';
 
 const app = express();
 
+app.get('/', (request, response) => {
+    console.log(request)
+    return response.status(234).send('Welcome To My MERN Stack App')
+});
+
+mongoose
+.connect(mongoDBURL)
+.then(() => {
+console.log('App connected to the database');
 app.listen(PORT, () => {
     console.log(`App is listening to port: ${PORT}`)
 });
+})
+.catch((error) => {
+console.log(error);
+})
