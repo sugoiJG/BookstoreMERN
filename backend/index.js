0108dc4eab46d07e78a363ad1405e198a67d8a9b
sugoiJG <jasongolliet@icloud.com>
2023-12-17 22:49:22 -0800

    finished adding routes to backend and now created React App with Vite and TailwindCSS

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,43 @@
-import express from 'express';
+import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
+import booksRoute from "./routes/booksRoute.js"
+import cors from 'cors'
 
 const app = express();
 
-app.get('/', (request, response) => {
-    console.log(request)
-    return response.status(234).send('Welcome To My MERN Stack App')
+//Middleware for parsing request body
+app.use(express.json());
+
+//Middleware for CORS 
+//OPTION 1: Allow All-origins with default of cors(*)
+// app.use(cors())
+
+//OPTION 2: Allow custom origins
+app.use(
+    cors({
+        origins: 'http://localhost:3000',
+        methods: ['GET','POST','PUT','DELETE'],
+        allowedHeaders: ['Content-Type'],
+    })
+)
+
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("Welcome To My MERN Stack App");
 });
 
+app.use('/books', booksRoute);
+
 mongoose
-.connect(mongoDBURL)
-.then(() => {
-console.log('App connected to the database');
-app.listen(PORT, () => {
-    console.log(`App is listening to port: ${PORT}`)
-});
-})
-.catch((error) => {
-console.log(error);
-})
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected to the database");
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
